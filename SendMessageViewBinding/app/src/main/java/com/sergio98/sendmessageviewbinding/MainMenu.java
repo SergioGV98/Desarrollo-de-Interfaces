34992513f044fe45e67070461632bed67058b61c
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-10-12 10:59:44 +0200

    a

--- SendMessageViewBinding/app/src/main/java/com/sergio98/sendmessageviewbinding/MainMenu.java
+++ SendMessageViewBinding/app/src/main/java/com/sergio98/sendmessageviewbinding/MainMenu.java
@@ -1,32 +1,39 @@
 package com.sergio98.sendmessageviewbinding;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.danielstone.materialaboutlibrary.MaterialAboutActivity;
-import com.danielstone.materialaboutlibrary.MaterialAboutFragment;
+import com.danielstone.materialaboutlibrary.items.MaterialAboutActionItem;
 import com.danielstone.materialaboutlibrary.items.MaterialAboutTitleItem;
 import com.danielstone.materialaboutlibrary.model.MaterialAboutCard;
 import com.danielstone.materialaboutlibrary.model.MaterialAboutList;
-import com.sergio98.sendmessageviewbinding.databinding.ActivitySendMessageBinding;
 
 public class MainMenu extends MaterialAboutActivity {
     @Override
     @NonNull
     protected MaterialAboutList getMaterialAboutList(@NonNull Context context) {
-        MaterialAboutCard.Builder cardBuilder = new MaterialAboutCard.Builder();
-        cardBuilder.addItem(new MaterialAboutTitleItem.Builder()
-                .text("Material About Library")
-                .icon(R.mipmap.ic_launcher)
+        MaterialAboutCard.Builder autor = new MaterialAboutCard.Builder();
+        autor.addItem(new MaterialAboutActionItem.Builder()
+                .text(R.string.nameCard)
+                .subText(R.string.descriptionCard)
+                .icon(R.drawable.ic_action_person)
+                .build());
+        autor.addItem(new MaterialAboutActionItem.Builder()
+                .text(R.string.githubCard)
+                .icon(R.drawable.ic_action_shared)
+                .build());
+
+        MaterialAboutCard.Builder version = new MaterialAboutCard.Builder();
+        version.addItem(new MaterialAboutActionItem.Builder()
+                .text(R.string.versionText)
+                .subText(R.string.versionInfo)
+                .icon(R.drawable.ic_action_info)
                 .build());
 
         return new MaterialAboutList.Builder()
-                .addCard(cardBuilder.build())
+                .addCard(autor.build())
+                .addCard(version.build())
                 .build();
     }
     @Nullable
