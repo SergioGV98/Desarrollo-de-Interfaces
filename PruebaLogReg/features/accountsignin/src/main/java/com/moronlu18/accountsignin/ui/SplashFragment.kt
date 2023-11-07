eb9281089b0e4aab5d7de198ce92c559f6c0b9e2
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-11-07 10:16:06 +0100

    a

---
+++ PruebaLogReg/features/accountsignin/src/main/java/com/moronlu18/accountsignin/ui/SplashFragment.kt
@@ -0,0 +1,43 @@
+package com.moronlu18.accountsignin.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.moronlu18.accountsignin.R
+import com.moronlu18.accountsignin.databinding.FragmentSplashBinding
+
+
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        var r = Runnable {
+            findNavController().navigate(R) //Poner el id de splash al main
+        }
+        Handler(Looper.getMainLooper()).postDelayed(r, WAI) // Crear el WAIT TIME
+    }
+}
+
+
--- PruebaLogReg/features/accountsignup/src/main/java/com/moronlu18/accountsignup/ui/AccountSignUp.kt
+++ PruebaLogReg/features/accountsignup/src/main/java/com/moronlu18/accountsignup/ui/AccountSignUp.kt
@@ -4,57 +4,53 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import com.moronlu18.accountsignup.R
+import com.moronlu18.accountsignup.databinding.FragmentAccountSignUpBinding
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AccountSignUp.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AccountSignUp : Fragment() {
 
-
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding : FragmentAccountSignUpBinding? = null
+    private val binding
+        get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_sign_up, container, false)
+        _binding = FragmentAccountSignUpBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AccountSignUp.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AccountSignUp().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    /**
+     * Se utiliza esta funcion para inicialziar los componentes que se han creado ya en la funcion
+     * onCreateView
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //1. Crear coleccion de datos
+        val itemList = arrayListOf("Private", "Public", "Empty")
+
+        //2. Crear adapter
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, itemList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        //binding.spProfile.adapter = adapter
+        //binding.spProfile.setSelection(2)
+
+        //3. Inicializar el listener que se lanza cuando el usuario modifica el valor
+        //Se usa el modismo With que dado un objeto se puede modificar propiedades dentro del bloque
+        with(binding.spProfile){
+            this.adapter = adapter
+            setSelection(2)
+            onItemSelectedListener = null
+        }
+
     }
 }
